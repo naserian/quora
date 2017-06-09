f6baac035d8cf607f4b76902eac791ced460f0e7
Chris Nyaga <nyagachris75@gmail.com>
2017-06-09 15:11:30 +0300

    Bug fixes

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:{
+    newQuestion:(function () {
+      var params = {
+        actualQuestion: this.get('actualQuestion'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      }
+      this.sendAction('saveNewQuestion',params)
+    })
+  }
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll('question')
+  },
+  actions:{
+    saveNewQuestion:function(params) {
+      var saveQuestion = this.store.createRecord('question',params);
+      saveQuestion.save();
+      this.transitionTo('index')
+    }
   }
 });
