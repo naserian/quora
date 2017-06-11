df0d5551aa29fcf92ea59ddac029a94e97788923
Chris Nyaga <nyagachris75@gmail.com>
2017-06-11 12:23:08 +0300

    Answer are now linked to only the question they are related to!

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question')
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
   actions:{
     saveNewQuestion(params){
+      //console.log(params);
       var newQuestion = this.store.createRecord('question',params);
       newQuestion.save();
       this.transitionTo('index');
-      console.log(params);
     }
   }
 });
